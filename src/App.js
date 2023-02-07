eb64c3a5f06cb44224faa2d41055da2b0d37ce52
Guilherme Avelino <gavelino@gmail.com>
2023-02-07 08:50:43 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aula GitHub Actions 2022-2
         </p>
         <a
           className="App-link"
